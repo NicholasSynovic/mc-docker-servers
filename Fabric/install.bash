6c1e44abcd2ec7bb1ab0ceb4f77c492eaaff1353
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-02-22 04:16:10 +0000

    Improve the installer

--- Fabric/install.bash
+++ Fabric/install.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 # NOTE: Requires a ngrok auth token as an arguement
+
 echo "Building Docker Container"
-docker build -t minecraft_server \
-  --build-arg fabric_download=https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.6.1.51/fabric-installer-0.6.1.51.jar \
-  --build-arg mc_version=1.16.5 \
-  --build-arg ngrok_authtoken=$1 \
-  --build-arg ngrok_download=https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip .
+DOCKER_BUILDKIT=1 docker build -t minecraft_server \
+  --build-arg tunnel=ngrok \
+  --build-arg ngrok_authtoken=$1
+
 echo "Running Docker Container"
 docker run -p25565:25565 --name minecraft minecraft_server
