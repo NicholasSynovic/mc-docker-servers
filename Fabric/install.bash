9efe5529513e7d09f52f3b8f36646b16953b2154
root <root@pop-os.localdomain>
2021-03-09 11:28:48 -0600

    Fix installer

--- Fabric/install.bash
+++ Fabric/install.bash
@@ -4,4 +4,4 @@ echo "Building Docker Container"
 docker build -t minecraft_server .
 
 echo "Running Docker Container"
-docker run -p25565:25565 --name minecraft minecraft_server
+docker run -p25565:25565 minecraft_server
